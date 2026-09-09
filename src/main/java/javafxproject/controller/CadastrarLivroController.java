190185c3ccdb3d0dbbaa1e84ac1448726045c163
alissonrv <alissonrochavidal@gmail.com>
2026-09-09 11:17:46 -0300

    criando o controller para a tela de cadastrara livros

--- src/main/java/javafxproject/controller/CadastrarLivroController.java
+++ src/main/java/javafxproject/controller/CadastrarLivroController.java
@@ -3,13 +3,69 @@ package javafxproject.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafxproject.HelloApplication;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class CadastrarLivroController {
+public class CadastrarLivroController implements Initializable {
+    @FXML
+    Button buttonCadastrar;
+    @FXML
+    TextField  tituloTextField;
+    @FXML
+    TextField autorTextField;
+    @FXML
+    TextField anoTextField;
+
+    private void mostrarAlerta(Alert.AlertType tipo, String titulo, String mensagem) {
+        Alert alert = new Alert(tipo);
+        alert.setTitle("");
+        alert.setHeaderText(titulo);
+        alert.setContentText(mensagem);
+        alert.showAndWait();
+    }
+
+    @FXML public void cadastrar() {
+        String titulo =  tituloTextField.getText().trim();   //se colocar o mesmo nome usar o this
+        String autor =  autorTextField.getText().trim();
+
+        if(titulo.isBlank() || autor.isBlank() || anoTextField.getText().isBlank()){   //isEmpty() = A string tem zero caracteres (""), isBlank()=A string tem zero caracteres ou só espaços em branco ("", " ", "\t", etc)
+            mostrarAlerta(
+                    Alert.AlertType.ERROR,
+                    "Erro ao cadastra dados!",
+                    "Preencha todos os campos!"
+            );
+            return;
+        }
+        try{
+            int ano =  Integer.parseInt(anoTextField.getText().trim());
+
+            mostrarAlerta(
+                    Alert.AlertType.INFORMATION,
+                    "",
+                    "Livro cadastrado com sucesso!"
+            );
+
+        }catch (NumberFormatException e){
+            mostrarAlerta(
+                    Alert.AlertType.ERROR,
+                    "Erro ao cadastrar Livro",
+                    "Digite apenas números no campo ano!"
+            );
+
+            return;
+        }
+
+//        Livro livro = new Livro(titulo, autor, ano);
+    }
 
     @FXML public void voltarMenu(ActionEvent event) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MenuView.fxml"));
@@ -17,5 +73,12 @@ public class CadastrarLivroController {
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setScene(scene);
         stage.show();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Restricoes.setTextFieldInteger(anoTextField);
+        Restricoes.setTextFieldMaxLength(anoTextField, 4);
+        Restricoes.setTextFieldDouble(anoTextField);
     }
 }
---
+++ src/main/java/javafxproject/controller/Restricoes.java
@@ -0,0 +1,29 @@
+package javafxproject.controller;
+
+import javafx.scene.control.TextField;
+
+public class Restricoes {
+    public static void setTextFieldInteger(TextField text){
+        text.textProperty().addListener((obs, oldValue, newValue) -> {
+            if(newValue != null && !newValue.matches("\\d*")){
+                text.setText(oldValue);
+            }
+        });
+    }
+
+    public static void setTextFieldMaxLength(TextField text, int maxLength){
+        text.textProperty().addListener((obs, oldValue, newValue) -> {
+            if(newValue != null && newValue.length() > maxLength){
+                text.setText(oldValue);
+            }
+        });
+    }
+
+    public static void setTextFieldDouble(TextField text){
+        text.textProperty().addListener((obs, oldValue, newValue) -> {
+            if(newValue != null && !newValue.matches("\\d*([\\.]\\d*)?")){
+                text.setText(oldValue);
+            }
+        });
+    }
+}
